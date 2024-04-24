567381789fa93066c87806583da7b7ee5b29ed25
alejandro.cano@imagineapps.co <alejandro.cano@imagineapps.co>
2024-04-23 22:40:58 -0500

    addinb billboard

---
+++ src/components/billboard/billboard-fixed-top/BillboardFixed.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import './billboard-fixed.scss'
+import ArrowDoubleSvg from '@/components/icons/ArrowDoubleSvg.vue'
+</script>
+
+<template>
+  <div class="billboard-fixed-container">
+
+    <div class="billboard-info">
+
+      <div class="title">
+        <h1>Elevate Your Sound</h1>
+      </div>
+
+      <div class="slogan">
+
+        <h2> Discover <span class="light">world</span>
+          <span class="span">class composers</span>
+        </h2>
+
+        <div class="arrow">
+          <ArrowDoubleSvg class="icon" />
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+  <img class="image-nillboard" src="/images/billboard/musicP.webp" alt="alt" />
+
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import BillboardComponent from '@/components/billboard/BillboardComponent.vue'
+import BillboardComponent from '@/components/billboard/billboard-fixed-top/BillboardFixed.vue'
 import ImageTitle from '@/components/sections/02-row/image-title/ImageTitle.vue'
 import CeoDescription from '@/components/sections/03-row/ceo-description/CeoDescription.vue'
 import ImageDescription from '@/components/sections/02-row/image-description/ImageDescription.vue'
