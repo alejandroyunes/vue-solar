c87423567cf4e4204b672c0cf43fe62983e62750
alejandro.cano@imagineapps.co <alejandroyunes@outlook.com>
2024-04-25 10:31:47 -0500

    added evetns

--- src/components/sections/04-row/UpcomingEvents.vue
+++ src/components/sections/04-row/UpcomingEvents.vue
@@ -11,14 +11,12 @@ import './upcoming-events.scss'
         <li class="list center">Woodstock Festival</li>
         <li class="list last">Tickets</li>
       </ul>
-
       <ul class="top">
         <li class="list">May 21 2024</li>
         <li class="list center">New York</li>
         <li class="list center">Radio City</li>
         <li class="list last">Tickets</li>
       </ul>
-
       <ul class="top">
         <li class="list">Jun 4 2024</li>
         <li class="list center">Washington</li>
