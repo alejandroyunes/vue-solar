3dca3e9bd387bb28bbcf84b26c8b82a9d65bd94c
alejandro.cano@imagineapps.co <alejandro.cano@imagineapps.co>
2024-04-25 10:30:18 -0500

    added evetns

---
+++ src/components/sections/04-row/UpcomingEvents.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import './upcoming-events.scss'
+</script>
+
+<template>
+  <section>
+    <div class="upcoming-events-container">
+      <ul class="top">
+        <li class="list">May 12 2024</li>
+        <li class="list center">Wisconsin</li>
+        <li class="list center">Woodstock Festival</li>
+        <li class="list last">Tickets</li>
+      </ul>
+
+      <ul class="top">
+        <li class="list">May 21 2024</li>
+        <li class="list center">New York</li>
+        <li class="list center">Radio City</li>
+        <li class="list last">Tickets</li>
+      </ul>
+
+      <ul class="top">
+        <li class="list">Jun 4 2024</li>
+        <li class="list center">Washington</li>
+        <li class="list center">DC Festival</li>
+        <li class="list last">Tickets</li>
+      </ul>
+    </div>
+  </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,6 +9,7 @@ import PartnersComponent from '@/components/sections/01-row/partners/PartnersCom
 import GetInTouch from '@/components/sections/02-row/get-in-touch/GetInTouch.vue'
 import IndentDescription from '@/components/sections/03-row/indent-description/IndentDescription.vue'
 import ImageTitleOverlay from '@/components/sections/03-row/images-title-overlay/ImagesTitleOverlay.vue'
+import UpcomingEvents from '@/components/sections/04-row/UpcomingEvents.vue'
 </script>
 
 <template>
@@ -20,6 +21,8 @@ import ImageTitleOverlay from '@/components/sections/03-row/images-title-overlay
   <TitleSubtitle title="Crafting Masterpieces" subtitle="Quality is our heartbeat" marginTop />
   <ImageTitleOverlay />
 
+  <TitleSubtitle title="Upcoming Events" subtitle="Tour Dates 2024" />
+  <UpcomingEvents />
 
   <ImageTitle />
   <CeoDescription />
