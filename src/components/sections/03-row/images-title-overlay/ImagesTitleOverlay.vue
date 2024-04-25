6e0e9a0f4d91dd4271e6319b9db53da6c9d5e749
alejandro.cano@imagineapps.co <alejandro.cano@imagineapps.co>
2024-04-25 10:00:28 -0500

    added images title overlay

--- src/components/sections/03-row/images-title-overlay/ImagesTitleOverlay.vue
+++ src/components/sections/03-row/images-title-overlay/ImagesTitleOverlay.vue
@@ -19,6 +19,29 @@ import './images-title-overlay.scss'
 
       </div>
 
+      <div class="item">
+        <img src="/images/sections/03-row/images-title-overlay/service2.webp" alt="alt" />
+
+        <div class="text-bg">
+          <div class="bg"></div>
+          <div class="text">
+            <p class="text-container">Innovation in Audio</p>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="item">
+        <img src="/images/sections/03-row/images-title-overlay/service3.webp" alt="alt" />
+
+        <div class="text-bg">
+          <div class="bg"></div>
+          <div class="text">
+            <p class="text-container">Live Vibe Creation</p>
+          </div>
+        </div>
+
+      </div>
     </div>
   </section>
 </template>
