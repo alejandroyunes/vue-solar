a1f697d801b25e9186abfaf19b940d1d8ea63447
alejandro.cano@imagineapps.co <alejandroyunes@outlook.com>
2024-04-25 19:04:31 -0500

    adding image grid

---
+++ src/components/sections/03-row/images-grid/ImageGrid.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import './image-grid.scss'
+</script>
+
+<template>
+
+  <section class="image-grid-container">
+
+    <div class="left-side">
+      <div>
+        <img class="first-image" src="/images/sections/03-row/grid-image/close-mic.webp" alt="" width={0} height={0} />
+      </div>
+
+      <div class="second-image">
+        <div>
+          <img class="image" src="/images/sections/03-row/grid-image/grid1.webp" alt="" width={0} height={0} />
+        </div>
+        <div>
+          <img class="image" src="/images/sections/03-row/grid-image/grid2.webp" alt="" width={0} height={0} />
+        </div>
+      </div>
+
+    </div>
+
+    <div>
+      <img class="billboard" src="/images/billboard/musicP.webp" alt="" width={0} height={0} />
+    </div>
+  </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,19 +10,22 @@ import GetInTouch from '@/components/sections/02-row/get-in-touch/GetInTouch.vue
 import IndentDescription from '@/components/sections/03-row/indent-description/IndentDescription.vue'
 import ImageTitleOverlay from '@/components/sections/03-row/images-title-overlay/ImagesTitleOverlay.vue'
 import UpcomingEvents from '@/components/sections/04-row/UpcomingEvents.vue'
+import ImageGrid from '@/components/sections/03-row/images-grid/ImageGrid.vue'
 </script>
 
 <template>
-  <BillboardComponent />
-
+  <!-- <BillboardComponent /> -->
+  <!-- 
   <TitleSubtitle title="Great quality for your tracks" subtitle="OUR MUSIC STUDIO" />
   <IndentDescription />
 
   <TitleSubtitle title="Crafting Masterpieces" subtitle="Quality is our heartbeat" marginTop />
   <ImageTitleOverlay />
+ -->
+  <!-- <TitleSubtitle title="Upcoming Events" subtitle="Tour Dates 2024" />
+  <UpcomingEvents /> -->
 
-  <TitleSubtitle title="Upcoming Events" subtitle="Tour Dates 2024" />
-  <UpcomingEvents />
+  <ImageGrid />
 
   <ImageTitle />
   <CeoDescription />
