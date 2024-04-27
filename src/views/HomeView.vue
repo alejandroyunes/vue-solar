187436cf13cfb27f2592a5b4345cd1ca66104f56
alejandro.cano@imagineapps.co <alejandroyunes@outlook.com>
2024-04-27 17:18:42 -0500

    finished music page 2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,6 @@
 <script setup lang="ts">
 import BillboardComponent from '@/components/billboard/billboard-fixed-top/BillboardFixed.vue'
 import TitleSubtitle from '@/components/sections/01-row/title-subtitle/TitleSubtitle.vue'
-
-import ImageTitle from '@/components/sections/02-row/image-title/ImageTitle.vue'
-import CeoDescription from '@/components/sections/03-row/ceo-description/CeoDescription.vue'
-import ImageDescription from '@/components/sections/02-row/image-description/ImageDescription.vue'
-import PartnersComponent from '@/components/sections/01-row/partners/PartnersComponent.vue'
-import GetInTouch from '@/components/sections/02-row/get-in-touch/GetInTouch.vue'
 import IndentDescription from '@/components/sections/03-row/indent-description/IndentDescription.vue'
 import ImageTitleOverlay from '@/components/sections/03-row/images-title-overlay/ImagesTitleOverlay.vue'
 import UpcomingEvents from '@/components/sections/04-row/UpcomingEvents.vue'
