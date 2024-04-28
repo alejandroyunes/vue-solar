d968810ec4afa28e12ee7b07b93a02566c787eaf
alejandro.cano@imagineapps.co <alejandroyunes@outlook.com>
2024-04-28 07:26:59 -0500

    fixing build issues

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,7 +11,7 @@ import SocialHover from '@/components/sections/06-row/social-hover/SocialHover.v
 <template>
   <BillboardComponent />
 
-  <TitleSubtitle title="Great quality for your tracks" subtitle="OUR MUSIC STUDIO" />
+  <TitleSubtitle title="Elevate your world with Sun-Powered Solutions" subtitle="OUR MUSIC STUDIO" />
   <IndentDescription />
 
   <TitleSubtitle title="Crafting Masterpieces" subtitle="Quality is our heartbeat" marginTop />
