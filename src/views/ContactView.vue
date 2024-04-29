f5f1fc8d72bc7a789459e08520e580bbda1afe98
alejandro.cano@imagineapps.co <alejandroyunes@outlook.com>
2024-04-29 05:49:33 -0500

    finished contact solr

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,12 +1,13 @@
 <script lang="ts" setup>
 import TitleArrow from '@/components/sections/01-row/title-arrow/TitleArrow.vue'
 import FooterMainIcons from '@/components/sections/03-row/footer-main-icons/FooterMainIcons.vue'
-import ImageGrid from '@/components/sections/03-row/images-grid/ImageGrid.vue'
+import ImageHeader from '@/components/sections/01-row/image-header/ImageHeader.vue';
 </script>
 
 <template>
   <TitleArrow :title="'Contact'" />
   <FooterMainIcons />
-  <ImageGrid />
+  <ImageHeader :imageUrl="'/images/contact/contact.webp'" :title="'Contact us today'" marginTop />
+
 
 </template>
